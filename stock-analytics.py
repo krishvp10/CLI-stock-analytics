025eccafdcf57f7871ca09c0d0e7ec639f7902bc
KrishPatel <krishvp10@gmail.com>
2026-05-30 21:36:15 +0530

    fix: seperate methods for fetching stock data, SMA, EMA and RSI

--- stock-analytics.py
+++ stock-analytics.py
@@ -5,14 +5,21 @@ class Stock:
         self.name = name               
         self.per = per
         self.avg = avg
+        
+    def fetch_data(self):    
         self.data = yf.download(self.name, period=f"{self.per}mo")
-        sma_list=(self.data[:self.avg]["Close"].values)
-        self.sma = sum(sma_list)/self.avg 
-        self.prev = (sum(sma_list[:-1]))/(self.avg-1)
+
+    def sma_data(self):    
+        self.sma_list=(self.data[:self.avg]["Close"].values)
+        self.sma = sum(self.sma_list)/self.avg
+
+    def ema_data(self): 
+        self.prev = (sum(self.sma_list[:-1]))/(self.avg-1)
         alpha=2/(self.avg + 1)
         current = self.data["Close"].iloc[self.avg].values
-        
         self.ema = (current * alpha) + (self.prev * (1 - alpha))
+        
+    def rsi_data(self):    
         rsi_list =(self.data[:14]["Close"].values)
         gain = []
         loss = []
@@ -27,12 +34,10 @@ class Stock:
         if(len(gain) != 0):
             avggain = sum(gain)/len(gain)
         else:
-            print("no gain")
             avggain = 0
         if(len(loss) != 0):
             avgloss = sum(loss)/len(loss)
         else:
-            print("no loss")
             avgloss = 0
         if(avgloss != 0):
             rs = avggain/avgloss
@@ -43,18 +48,22 @@ class Stock:
             self.rsa = 100
         
 
-            
-        
     def summary(self):
         print(f"the stock you entered is: {self.name}")
         print(f"the data for the duration of {self.per} months is : \n{self.data}")
         print(f"the sma of {self.name} for {self.avg} days is: {self.sma}")
         print(f"the ema would be : {self.ema}")
         print(f"the 14 days rsi of the stock {self.name} is : {self.rsa}")
+
+
 s1=input("enter the symbol of the stock you want the data for:")
 time=int(input("enter the time duration in months "))
 avg=int(input("enter the number of days you want the sma and ema for"))
 stock1 = Stock(s1,time,avg)
+stock1.fetch_data()
+stock1.sma_data()
+stock1.ema_data()
+stock1.rsi_data()
 stock1.summary()
                   
 
